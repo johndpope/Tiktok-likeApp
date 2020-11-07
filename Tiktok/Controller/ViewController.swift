314aa7fd526b12204b351f3cd587928289a8f854
yoshihiro.uda <yoshihiro7319@yahoo.co.jp>
2020-11-08 00:29:30 +0900

    create UI

---
+++ Tiktok/Controller/ViewController.swift
@@ -0,0 +1,34 @@
+//
+//  ViewController.swift
+//  Tiktok
+//
+//  Created by Yoshihiro Uda on 2020/11/08.
+//
+
+import UIKit
+import MobileCoreServices
+
+
+class ViewController: UIViewController {
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+      
+        
+        
+    }
+    
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        self.navigationController?.isNavigationBarHidden = true
+    }
+ 
+    
+    
+    
+
+}
