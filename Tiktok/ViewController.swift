eade81f1d983a850e41ffdabff7a3487810c74ec
yoshihiro.uda <yoshihiro7319@yahoo.co.jp>
2020-11-07 23:56:24 +0900

    Initial Commit

---
+++ Tiktok/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Tiktok
+//
+//  Created by Yoshihiro Uda on 2020/11/07.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
