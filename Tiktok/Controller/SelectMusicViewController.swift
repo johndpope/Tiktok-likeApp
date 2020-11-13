7271e922f4e670566cd391d8bc93f0d3e65b8b97
yoshihiro.uda <yoshihiro7319@yahoo.co.jp>
2020-11-14 01:30:55 +0900

    create Buttons

--- Tiktok/Controller/SelectMusicViewController.swift
+++ Tiktok/Controller/SelectMusicViewController.swift
@@ -16,6 +16,7 @@ class SelectViewController: UIViewController,UITableViewDelegate,UITableViewData
     @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var searchTextField: UITextField!
     
+    var musicModel = MusicModel()
     var player:AVAudioPlayer?
     var videoPath = String()
     var passedURL:URL?
@@ -31,15 +32,68 @@ class SelectViewController: UIViewController,UITableViewDelegate,UITableViewData
         
     }
     
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
 
-  
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        <#code#>
+        
+        return musicModel.artistNameArray.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        <#code#>
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        cell.isHighlighted = false
+        
+        let artWorkImageView = cell.contentView.viewWithTag(1) as! UIImageView
+        let musicNameLabel = cell.contentView.viewWithTag(2) as! UILabel
+        let artistNameLabel = cell.contentView.viewWithTag(3) as! UILabel
+        
+        artWorkImageView.sd_setImage(with: URL(string: musicModel.artworkUrl100Array[indexPath.row]), completed: nil)
+        musicNameLabel.text = musicModel.trackCensoredNameArray[indexPath.row]
+        artistNameLabel.text = musicModel.artistNameArray[indexPath.row]
+        
+        //プログラムでボタン作成(ラベルの横)
+        let favButton = UIButton(frame: CGRect(x: 330, y: 39, width: 40, height: 33))
+        favButton.setImage(UIImage(named: "fav"), for: .normal)
+        favButton.addTarget(self, action: #selector(favButtonTap(_:)), for: .touchUpInside)
+        favButton.tag = indexPath.row
+        cell.contentView.addSubview(favButton)
+        
+        //プログラムでボタン作成(ImageViewの上)
+        let playButton = UIButton(frame: CGRect(x: 20, y: 5, width: 100, height: 100))
+        playButton.setImage(UIImage(named: "play"), for: .normal)
+        playButton.addTarget(self, action: #selector(playButtonTap(_:)), for: .touchUpInside)
+        playButton.tag = indexPath.row
+        cell.contentView.addSubview(playButton)
+        
+        return cell
     }
+    
+    
+    @objc func playButtonTap(_ sender:UIButton){
+        
+        //音楽を止める
+        if player?.isPlaying == true{
+            
+            player?.stop()
+        }
+        
+        let url = URL(string: musicModel.preViewUrlArray[sender.tag])
+        
+    }
+    
+    
+    func downLoadMusicURL(url:URL){
+        
+        
+        
+        
+    }
+    
     
     
     func refleshData(){
@@ -49,6 +103,8 @@ class SelectViewController: UIViewController,UITableViewDelegate,UITableViewData
             let urlString = "http;//itunes.apple.com/search?term=\(String(describing:searchTextField.text))&entity=song&country=jp"
             
             let encordUrlString:String = urlString.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed)!
+            musicModel.setData(resultCount: 50, encodeUrlString: encordUrlString)
+            searchTextField.resignFirstResponder()
             
         }
         
--- Tiktok/Model/MusicModel.swift
+++ Tiktok/Model/MusicModel.swift
@@ -11,14 +11,63 @@ import Alamofire
 
 class MusicModel{
     
-    //アーティスト名
-    var artistName:String?
-    //曲名
-    var
+    //    //アーティスト名
+    //    var artistName:String?
+    //    //曲名
+    //    var trackCensoredName:String?
+    //    //音源URL
+    //    var preViewUrl:String?
+    //    //ジャケット
+    //    var artworkUrl100:String?
     
-    
-    //通信
+    var artistNameArray = [String]()
+    var trackCensoredNameArray = [String]()
+    var preViewUrlArray = [String]()
+    var artworkUrl100Array = [String]()
     
     //JSON解析
+    
+    func setData(resultCount:Int, encodeUrlString:String){
+        
+        //通信
+        AF.request(encodeUrlString, method: .get, parameters: nil, encoding: JSONEncoding.default).responseJSON { (response) in
+            
+            //まず配列を空にする
+            self.artistNameArray.removeAll()
+            self.trackCensoredNameArray.removeAll()
+            self.preViewUrlArray.removeAll()
+            self.artworkUrl100Array.removeAll()
+            
+            switch response.result{
+            
+            case .success:
+                
+                do {
+                    let json:JSON = try JSON(data: response.data!)
+                    
+                    for i in 0...resultCount - 1{
+                        
+                        //ヒットしないものが検索された場合
+                        if json["results"][i]["artistName"].string == nil{
+                            print("ヒットしませんでした。")
+                            return
+                        }
+                        
+                        self.artistNameArray.append(json["results"][i]["artistName"].string!)
+                        self.trackCensoredNameArray.append(json["results"][i]["trackCensoredName"].string!)
+                        self.preViewUrlArray.append(json["results"][i]["artistViewUrl"].string!)
+                        self.artworkUrl100Array.append(json["results"][i]["artworkUrl100"].string!)
+                        
+                    }
+                } catch {
+                }
+                break
+                
+            case .failure(_): break
+                
+            }
+            
+        }
+    }
     
 }
